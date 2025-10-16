c6b56f6beee79cf498a2fdac71b107da30b1429c
RahulVerma2629 <167573663+RahulVerma2629@users.noreply.github.com>
2025-10-16 23:36:19 +0530

    Create Que1.cpp

---
+++ Array2/Que1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int arr[] = {5,1,3,4,2,6};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  int T = 5;
+  for(int i=0; i<n; i++){
+  	if(arr[i]==T){
+    	cout<<"Element Found at Position: "<<i;
+    }
+  }
+}
