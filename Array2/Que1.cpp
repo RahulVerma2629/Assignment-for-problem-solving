35f44f835242d8a47a8fe921432990695e167cf5
RahulVerma2629 <167573663+RahulVerma2629@users.noreply.github.com>
2025-10-17 00:30:58 +0530

    Update Que1.cpp

--- Array2/Que1.cpp
+++ Array2/Que1.cpp
@@ -4,6 +4,7 @@ int main(){
   int arr[] = {5,1,3,4,2,6};
   int n = sizeof(arr)/sizeof(arr[0]);
   int T = 5;
+  // simple linear search
   for(int i=0; i<n; i++){
   	if(arr[i]==T){
     	cout<<"Element Found at Position: "<<i;
