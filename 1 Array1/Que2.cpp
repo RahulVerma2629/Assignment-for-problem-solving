f615bb57dbce72731e0add4b15eb1bc0dbcfd6df
RahulVerma2629 <167573663+RahulVerma2629@users.noreply.github.com>
2025-10-17 17:03:18 +0530

    Update Que2.cpp

--- 1 Array1/Que2.cpp
+++ 1 Array1/Que2.cpp
@@ -3,21 +3,37 @@ public:
     int minimumDeletions(vector<int>& nums) {
         int n = nums.size();
         int mn = INT_MAX, mx = INT_MIN;
-        int mindx = 0, maxdx = 0;
+        int minI = 0, maxI = 0;
         for(int i=0; i<n; i++){
-            if(nums[i]>mx){
-                mx = nums[i]; 
-                maxdx = i; 
-            }
             if(nums[i]<mn){
-                mn = nums[i]; 
-                mindx = i; 
+                mn = nums[i];
+                minI = i;
             }
         }
-        if(mindx>maxdx) swap(mindx,maxdx);
-        int left = maxdx+1;       
-        int right = n-mindx; 
-        int both = (mindx+1)+(n-maxdx);
-        return min({left, right, both});
+        for(int j=0; j<n; j++){
+            if(nums[j]>mx){
+                mx = nums[j];
+                maxI = j;
+            }
+        }
+        int count1 = 0, count2 = 0, count3 = 0;
+        // left se count
+        for(int i=0; i<=max(minI,maxI); i++){
+            count1++;
+        }
+        // right se count
+        for(int i=n-1; i>=min(minI,maxI); i--){
+            count2++;
+        }
+        // dono taraf se count
+        for(int i=0; i<=min(minI,maxI); i++){
+            count3++;
+        }
+        for(int j=n-1; j>=max(minI,maxI); j--){
+            count3++;
+        }
+        if(count1<=count2 && count1<=count3) return count1;
+        else if(count2<=count1 && count2<=count3) return count2;
+        return count3;
     }
 };
